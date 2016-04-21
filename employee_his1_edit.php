b05c9d4e50e5991248bf5cc64e1c32d138a16346
SohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2016-04-21 09:06:55 -0700

    Added files via upload

---
+++ employee_his1_edit.php
@@ -0,0 +1,360 @@
+<?php 
+
+@ini_set("display_errors","1");
+@ini_set("display_startup_errors","1");
+
+require_once("include/dbcommon.php");
+require_once("include/employee_his1_variables.php");
+require_once('include/xtempl.php');
+require_once('classes/editpage.php');
+require_once("classes/searchclause.php");
+
+add_nocache_headers();
+
+if( !EditPage::processEditPageSecurity( $strTableName ) )
+	return;
+
+EditPage::handleBrokenRequest();
+
+//	render all necessary layouts
+
+
+
+
+$layout = new TLayout("edit21", "Office1Avenue2", "MobileAvenue2");
+$layout->version = 2;
+$layout->blocks["top"] = array();
+$layout->containers["hmenu"] = array();
+$layout->container_properties["hmenu"] = array(  );
+$layout->containers["hmenu"][] = array("name"=>"hmenu", 
+	"block"=>"menu_block", "substyle"=>1  );
+
+$layout->skins["hmenu"] = "undermenu";
+
+$layout->blocks["top"][] = "hmenu";
+$layout->containers["all"] = array();
+$layout->container_properties["all"] = array(  );
+$layout->containers["all"][] = array("name"=>"wrapper", 
+	"block"=>"", "substyle"=>1 , "container"=>"edit" );
+$layout->containers["edit"] = array();
+$layout->container_properties["edit"] = array(  );
+$layout->containers["edit"][] = array("name"=>"editheader", 
+	"block"=>"editheader", "substyle"=>2  );
+
+$layout->containers["edit"][] = array("name"=>"message", 
+	"block"=>"message_block", "substyle"=>1  );
+
+$layout->containers["edit"][] = array("name"=>"wrapper", 
+	"block"=>"", "substyle"=>1 , "container"=>"fields" );
+$layout->containers["fields"] = array();
+$layout->container_properties["fields"] = array(  );
+$layout->containers["fields"][] = array("name"=>"editfields", 
+	"block"=>"", "substyle"=>1  );
+
+$layout->containers["fields"][] = array("name"=>"editbuttons", 
+	"block"=>"editbuttons", "substyle"=>2  );
+
+$layout->skins["fields"] = "fields";
+
+
+$layout->skins["edit"] = "1";
+
+
+$layout->skins["all"] = "empty";
+
+$layout->blocks["top"][] = "all";
+$page_layouts["employee_his1_edit"] = $layout;
+
+$layout->skinsparams = array();
+$layout->skinsparams["empty"] = array("button"=>"button2");
+$layout->skinsparams["menu"] = array("button"=>"button1");
+$layout->skinsparams["hmenu"] = array("button"=>"button1");
+$layout->skinsparams["undermenu"] = array("button"=>"button1");
+$layout->skinsparams["fields"] = array("button"=>"button1");
+$layout->skinsparams["form"] = array("button"=>"button1");
+$layout->skinsparams["1"] = array("button"=>"button1");
+$layout->skinsparams["2"] = array("button"=>"button1");
+$layout->skinsparams["3"] = array("button"=>"button1");
+
+
+
+
+
+
+$layout = new TLayout("tab", "Office1Avenue2", "MobileAvenue2");
+$layout->version = 2;
+$layout->blocks["bare"] = array();
+$layout->containers["1"] = array();
+$layout->container_properties["1"] = array(  );
+$layout->containers["1"][] = array("name"=>"addtabfield", 
+	"block"=>"", "substyle"=>1  );
+
+$layout->skins["1"] = "empty";
+
+$layout->blocks["bare"][] = "1";
+$page_layouts["employee_his1_edit_________________1"] = $layout;
+
+$layout->skinsparams = array();
+$layout->skinsparams["empty"] = array("button"=>"button2");
+$layout->skinsparams["menu"] = array("button"=>"button1");
+$layout->skinsparams["hmenu"] = array("button"=>"button1");
+$layout->skinsparams["undermenu"] = array("button"=>"button1");
+$layout->skinsparams["fields"] = array("button"=>"button1");
+$layout->skinsparams["form"] = array("button"=>"button1");
+$layout->skinsparams["1"] = array("button"=>"button1");
+$layout->skinsparams["2"] = array("button"=>"button1");
+$layout->skinsparams["3"] = array("button"=>"button1");
+
+
+
+
+
+$layout = new TLayout("tab", "Office1Avenue2", "MobileAvenue2");
+$layout->version = 2;
+$layout->blocks["bare"] = array();
+$layout->containers["1"] = array();
+$layout->container_properties["1"] = array(  );
+$layout->containers["1"][] = array("name"=>"addtabfield", 
+	"block"=>"", "substyle"=>1  );
+
+$layout->skins["1"] = "empty";
+
+$layout->blocks["bare"][] = "1";
+$page_layouts["employee_his1_edit___________________________1"] = $layout;
+
+$layout->skinsparams = array();
+$layout->skinsparams["empty"] = array("button"=>"button2");
+$layout->skinsparams["menu"] = array("button"=>"button1");
+$layout->skinsparams["hmenu"] = array("button"=>"button1");
+$layout->skinsparams["undermenu"] = array("button"=>"button1");
+$layout->skinsparams["fields"] = array("button"=>"button1");
+$layout->skinsparams["form"] = array("button"=>"button1");
+$layout->skinsparams["1"] = array("button"=>"button1");
+$layout->skinsparams["2"] = array("button"=>"button1");
+$layout->skinsparams["3"] = array("button"=>"button1");
+
+
+
+
+
+$layout = new TLayout("tab", "Office1Avenue2", "MobileAvenue2");
+$layout->version = 2;
+$layout->blocks["bare"] = array();
+$layout->containers["1"] = array();
+$layout->container_properties["1"] = array(  );
+$layout->containers["1"][] = array("name"=>"addtabfield", 
+	"block"=>"", "substyle"=>1  );
+
+$layout->skins["1"] = "empty";
+
+$layout->blocks["bare"][] = "1";
+$page_layouts["employee_his1_edit____________________1"] = $layout;
+
+$layout->skinsparams = array();
+$layout->skinsparams["empty"] = array("button"=>"button2");
+$layout->skinsparams["menu"] = array("button"=>"button1");
+$layout->skinsparams["hmenu"] = array("button"=>"button1");
+$layout->skinsparams["undermenu"] = array("button"=>"button1");
+$layout->skinsparams["fields"] = array("button"=>"button1");
+$layout->skinsparams["form"] = array("button"=>"button1");
+$layout->skinsparams["1"] = array("button"=>"button1");
+$layout->skinsparams["2"] = array("button"=>"button1");
+$layout->skinsparams["3"] = array("button"=>"button1");
+
+
+
+
+
+$layout = new TLayout("tab", "Office1Avenue2", "MobileAvenue2");
+$layout->version = 2;
+$layout->blocks["bare"] = array();
+$layout->containers["1"] = array();
+$layout->container_properties["1"] = array(  );
+$layout->containers["1"][] = array("name"=>"addtabfield", 
+	"block"=>"", "substyle"=>1  );
+
+$layout->skins["1"] = "empty";
+
+$layout->blocks["bare"][] = "1";
+$page_layouts["employee_his1_edit_______________1"] = $layout;
+
+$layout->skinsparams = array();
+$layout->skinsparams["empty"] = array("button"=>"button2");
+$layout->skinsparams["menu"] = array("button"=>"button1");
+$layout->skinsparams["hmenu"] = array("button"=>"button1");
+$layout->skinsparams["undermenu"] = array("button"=>"button1");
+$layout->skinsparams["fields"] = array("button"=>"button1");
+$layout->skinsparams["form"] = array("button"=>"button1");
+$layout->skinsparams["1"] = array("button"=>"button1");
+$layout->skinsparams["2"] = array("button"=>"button1");
+$layout->skinsparams["3"] = array("button"=>"button1");
+
+
+
+
+
+$layout = new TLayout("tab", "Office1Avenue2", "MobileAvenue2");
+$layout->version = 2;
+$layout->blocks["bare"] = array();
+$layout->containers["1"] = array();
+$layout->container_properties["1"] = array(  );
+$layout->containers["1"][] = array("name"=>"addtabfield", 
+	"block"=>"", "substyle"=>1  );
+
+$layout->skins["1"] = "empty";
+
+$layout->blocks["bare"][] = "1";
+$page_layouts["employee_his1_edit_____________________________1"] = $layout;
+
+$layout->skinsparams = array();
+$layout->skinsparams["empty"] = array("button"=>"button2");
+$layout->skinsparams["menu"] = array("button"=>"button1");
+$layout->skinsparams["hmenu"] = array("button"=>"button1");
+$layout->skinsparams["undermenu"] = array("button"=>"button1");
+$layout->skinsparams["fields"] = array("button"=>"button1");
+$layout->skinsparams["form"] = array("button"=>"button1");
+$layout->skinsparams["1"] = array("button"=>"button1");
+$layout->skinsparams["2"] = array("button"=>"button1");
+$layout->skinsparams["3"] = array("button"=>"button1");
+
+
+
+
+
+$layout = new TLayout("tab", "Office1Avenue2", "MobileAvenue2");
+$layout->version = 2;
+$layout->blocks["bare"] = array();
+$layout->containers["1"] = array();
+$layout->container_properties["1"] = array(  );
+$layout->containers["1"][] = array("name"=>"addtabfield", 
+	"block"=>"", "substyle"=>1  );
+
+$layout->skins["1"] = "empty";
+
+$layout->blocks["bare"][] = "1";
+$page_layouts["employee_his1_edit_____________1"] = $layout;
+
+$layout->skinsparams = array();
+$layout->skinsparams["empty"] = array("button"=>"button2");
+$layout->skinsparams["menu"] = array("button"=>"button1");
+$layout->skinsparams["hmenu"] = array("button"=>"button1");
+$layout->skinsparams["undermenu"] = array("button"=>"button1");
+$layout->skinsparams["fields"] = array("button"=>"button1");
+$layout->skinsparams["form"] = array("button"=>"button1");
+$layout->skinsparams["1"] = array("button"=>"button1");
+$layout->skinsparams["2"] = array("button"=>"button1");
+$layout->skinsparams["3"] = array("button"=>"button1");
+
+
+
+
+
+$layout = new TLayout("tab", "Office1Avenue2", "MobileAvenue2");
+$layout->version = 2;
+$layout->blocks["bare"] = array();
+$layout->containers["1"] = array();
+$layout->container_properties["1"] = array(  );
+$layout->containers["1"][] = array("name"=>"addtabfield", 
+	"block"=>"", "substyle"=>1  );
+
+$layout->skins["1"] = "empty";
+
+$layout->blocks["bare"][] = "1";
+$page_layouts["employee_his1_edit_________1"] = $layout;
+
+$layout->skinsparams = array();
+$layout->skinsparams["empty"] = array("button"=>"button2");
+$layout->skinsparams["menu"] = array("button"=>"button1");
+$layout->skinsparams["hmenu"] = array("button"=>"button1");
+$layout->skinsparams["undermenu"] = array("button"=>"button1");
+$layout->skinsparams["fields"] = array("button"=>"button1");
+$layout->skinsparams["form"] = array("button"=>"button1");
+$layout->skinsparams["1"] = array("button"=>"button1");
+$layout->skinsparams["2"] = array("button"=>"button1");
+$layout->skinsparams["3"] = array("button"=>"button1");
+
+
+
+
+
+$layout = new TLayout("tab", "Office1Avenue2", "MobileAvenue2");
+$layout->version = 2;
+$layout->blocks["bare"] = array();
+$layout->containers["1"] = array();
+$layout->container_properties["1"] = array(  );
+$layout->containers["1"][] = array("name"=>"addtabfield", 
+	"block"=>"", "substyle"=>1  );
+
+$layout->skins["1"] = "empty";
+
+$layout->blocks["bare"][] = "1";
+$page_layouts["employee_his1_edit____________1"] = $layout;
+
+$layout->skinsparams = array();
+$layout->skinsparams["empty"] = array("button"=>"button2");
+$layout->skinsparams["menu"] = array("button"=>"button1");
+$layout->skinsparams["hmenu"] = array("button"=>"button1");
+$layout->skinsparams["undermenu"] = array("button"=>"button1");
+$layout->skinsparams["fields"] = array("button"=>"button1");
+$layout->skinsparams["form"] = array("button"=>"button1");
+$layout->skinsparams["1"] = array("button"=>"button1");
+$layout->skinsparams["2"] = array("button"=>"button1");
+$layout->skinsparams["3"] = array("button"=>"button1");
+
+
+
+
+// parse control parameters
+$pageMode = EditPage::readEditModeFromRequest();
+
+$xt = new Xtempl();	
+	
+$id = postvalue("id");
+$id = intval($id) == 0 ? 1 : $id;
+
+
+// $keys could not be set properly if editid params were no passed
+$keys = array();
+$keys["Id"] = postvalue("editid1");
+
+//array of params for classes
+$params = array();
+$params["id"] = $id;
+$params["xt"] = &$xt;
+$params["keys"] = $keys;
+$params["mode"] = $pageMode;
+$params["pageType"] = PAGE_EDIT;
+$params["tName"] = $strTableName;
+$params["action"] = postvalue("a");
+
+//	locking parameters
+$params["lockingAction"] = postvalue("action");
+$params["lockingSid"] = postvalue("sid");
+$params["lockingKeys"] = postvalue("keys");
+$params["lockingStart"] = postvalue("startEdit");
+
+if( $pageMode == EDIT_INLINE )
+{
+	$params["screenWidth"] = postvalue("screenWidth");
+	$params["screenHeight"] = postvalue("screenHeight");
+	$params["orientation"] = postvalue("orientation");
+}	
+
+if( $pageMode == EDIT_DASHBOARD ) 
+{
+	$params["dashElementName"] = postvalue("dashelement");
+	$params["dashTName"] = postvalue("table");
+}
+
+$pageObject = new EditPage($params);
+$pageObject->init();
+
+if( $pageObject->isLockingRequest() )
+{
+	$pageObject->doLockingAction();
+	exit();
+}
+
+$pageObject->process();
+
+?>
